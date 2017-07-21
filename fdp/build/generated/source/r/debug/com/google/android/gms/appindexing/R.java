50943d70eb63136372bc0132763a219cb7b72cb9
Julian Gómez <jgomez@grameenfoundation.org>
2017-07-21 14:20:55 -0500

    Finish french translation and add family expenses FCFA 2'000.000 provide by Bilal 21/07/2017

--- fdp/build/generated/source/r/debug/com/google/android/gms/appindexing/R.java
+++ fdp/build/generated/source/r/debug/com/google/android/gms/appindexing/R.java
@@ -39,8 +39,8 @@ public final class R {
         public static final int google_play_services_version = 0x7f0b02a7;
     }
     public static final class string {
-        public static final int auth_google_play_services_client_facebook_display_name = 0x7f060035;
-        public static final int auth_google_play_services_client_google_display_name = 0x7f060036;
+        public static final int auth_google_play_services_client_facebook_display_name = 0x7f060102;
+        public static final int auth_google_play_services_client_google_display_name = 0x7f060103;
         public static final int common_android_wear_notification_needs_update_text = 0x7f060000;
         public static final int common_android_wear_update_text = 0x7f060001;
         public static final int common_android_wear_update_title = 0x7f060002;
