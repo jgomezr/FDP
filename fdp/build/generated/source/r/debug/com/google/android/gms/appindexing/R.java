0f5efdd8c1f7fd2a09e27622c87015f94e8577f3
Julian Gómez <jgomez@grameenfoundation.org>
2017-10-02 12:10:04 -0500

    French translations for Monthly activities

--- fdp/build/generated/source/r/debug/com/google/android/gms/appindexing/R.java
+++ fdp/build/generated/source/r/debug/com/google/android/gms/appindexing/R.java
@@ -39,8 +39,8 @@ public final class R {
         public static final int google_play_services_version = 0x7f0b044d;
     }
     public static final class string {
-        public static final int auth_google_play_services_client_facebook_display_name = 0x7f06013d;
-        public static final int auth_google_play_services_client_google_display_name = 0x7f06013e;
+        public static final int auth_google_play_services_client_facebook_display_name = 0x7f06013e;
+        public static final int auth_google_play_services_client_google_display_name = 0x7f06013f;
         public static final int common_android_wear_notification_needs_update_text = 0x7f060000;
         public static final int common_android_wear_update_text = 0x7f060001;
         public static final int common_android_wear_update_title = 0x7f060002;
